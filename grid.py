c230054f53a4f6edc57b6ff1eb7621d776de7049
Kyle Tran <kyletran0621@gmail.com>
2021-06-03 21:52:58 -0500

    Implemented ternary cellular automata; fixed random field generation option

--- grid.py
+++ grid.py
@@ -3,7 +3,7 @@ import numpy as np
 import random
 
 
-class Grid:
+class Conway:
     def __init__(self, width, height, scale, border):
         self.scale = scale
 
@@ -11,7 +11,7 @@ class Grid:
         self.columns = int(height / scale)
         self.size = (self.rows, self.columns)
 
-        self.curr_array = np.ndarray(shape=self.size) # Field as 2d array
+        self.curr_array = np.ndarray(shape=self.size)  # Field as 2d array
         self.border = border  # Lines between cells
 
     def update(self, dead, live, surface):
@@ -78,5 +78,88 @@ class Grid:
         # Clears entire board to all dead cells
         for x in range(self.rows):
             for y in range(self.columns):
-                 self.curr_array[x][y] = 0
+                self.curr_array[x][y] = 0
 
+
+class RPS:
+    # Rock = -1, White = 0, Paper = 1, Scissors = 2
+    def __init__(self, width, height, scale, border):
+        self.scale = scale
+
+        self.rows = int(width / scale)
+        self.columns = int(height / scale)
+        self.size = (self.rows, self.columns)
+
+        self.curr_array = np.ndarray(shape=self.size)  # Field as 2d array
+        self.border = border  # Lines between cells
+
+    def update(self, rock, paper, scissors, surface):
+        # updates cells to be dead or live
+        for x in range(self.rows):
+            for y in range(self.columns):
+                x_pos, y_pos = x * self.scale, y * self.scale
+                if -1 < self.curr_array[x][y] < 1: #Fix rounding error near 0
+                    pygame.draw.rect(surface, (255, 255, 255),
+                                     [x_pos, y_pos, self.scale - self.border, self.scale - self.border])
+                elif self.curr_array[x][y] == -1:
+                    pygame.draw.rect(surface, rock,
+                                     [x_pos, y_pos, self.scale - self.border, self.scale - self.border])
+                elif self.curr_array[x][y] == 1:
+                    pygame.draw.rect(surface, paper,
+                                     [x_pos, y_pos, self.scale - self.border, self.scale - self.border])
+                else:
+                    pygame.draw.rect(surface, scissors,
+                                     [x_pos, y_pos, self.scale - self.border, self.scale - self.border])
+
+    def transition(self):
+        # rules for transitions between generations
+        new_array = np.ndarray(shape=self.size)
+        for x in range(self.rows):
+            for y in range(self.columns):
+                state = self.curr_array[x][y]
+                neighbors = self.get_neighbors(x, y, state)
+                if neighbors > 2:
+                    if state == -1:
+                        new_array[x][y] = 1
+                    elif state == 1:
+                        new_array[x][y] = 2
+                    else:
+                        new_array[x][y] = -1
+                else:
+                    new_array[x][y] = state
+
+        # update previous field with next generation's field
+        self.curr_array = new_array
+
+    def get_neighbors(self, x, y, state):
+        total = 0
+        # check 8 cells around current cell
+        for n in range(-1, 2):
+            for m in range(-1, 2):
+                if not (n == m == 0):  # Ignore self during check
+                    # Since field is finite, stitch edges to yield toroidal array
+                    x_edge = (x + n + self.rows) % self.rows
+                    y_edge = (y + m + self.columns) % self.columns
+                    neighbor = self.curr_array[x_edge][y_edge]
+                    if (state == -1 and neighbor == 1) or (state == 1 and neighbor == 2) \
+                            or (state == 2 and neighbor == -1):
+                        total += 1
+        return total
+
+    def click(self, pos, choice):
+        # Clicking on cell will change it's state from dead to live or vice versa
+        x, y = int(pos[0] / self.scale), int(pos[1] / self.scale)
+        self.curr_array[x][y] = choice
+
+    def random_field(self):
+        # Generates random field of cells
+        for x in range(self.rows):
+            for y in range(self.columns):
+                #self.curr_array[x][y] = random.randint(0, 1)
+                self.curr_array[x][y] = random.choices([-1, 1, 2])[0]
+
+    def reset(self):
+        # Clears entire board to all dead cells
+        for x in range(self.rows):
+            for y in range(self.columns):
+                self.curr_array[x][y] = 0
--- main.py
+++ main.py
@@ -8,30 +8,37 @@ pygame.init()
 info = pygame.display.Info()
 width, height = info.current_w, info.current_h - 50
 size = (width, height)
-screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
-# screen = pygame.display.set_mode(size, pygame.RESIZABLE)
 
 # Create Grid
 scalar = 40
-border = 1
-Grid = grid.Grid(width, height, scalar, border)
-# Grid.random_field()
+border = 0
+conway = grid.Conway(width, height, scalar, border)
+rps = grid.RPS(width, height, scalar, border)
 
 # Colors
 black = (0, 0, 0)
 blue = (1, 33, 105)
 white = (255, 255, 255)
 
+orange = (255, 127, 0)
+light_blue = (31, 120, 180)
+red = (228, 26, 28)
+
 # speed between generations
 clock = pygame.time.Clock()
-speed = 30
+speed = 60
 
-# Initialize field
-screen.fill(black)
-Grid.update(dead=white, live=blue, surface=screen)
-Grid.transition()
 
-def game():
+def conway_game(random):
+    # Initialize field
+    screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
+    # screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+    screen.fill(black)
+    conway.update(dead=white, live=blue, surface=screen)
+    conway.transition()
+    if random == "y" or random == "Y":
+        conway.random_field()
+
     generation = 0
     while True:
         pygame.display.set_caption("Conway's Game Of Life - Generation " + str(generation))
@@ -40,12 +47,12 @@ def game():
 
         # Continually iterates through generations while space is held
         if keys[pygame.K_SPACE]:
-            Grid.transition()
+            conway.transition()
             generation += 1
 
         # Resets board and generation to 0 using command Ctrl + R
         if (keys[pygame.K_LCTRL] or keys[pygame.K_RCTRL]) and keys[pygame.K_r]:
-            Grid.reset()
+            conway.reset()
             generation = 0
             pygame.display.set_caption("Conway's Game Of Life - Generation " + str(generation))
 
@@ -54,17 +61,77 @@ def game():
                 button = event.button
                 if button == 1:  # change cell state with left click
                     pos = pygame.mouse.get_pos()
-                    Grid.click(pos)
+                    conway.click(pos)
                 elif button == 3:  # iterate through next generation once right click
-                    Grid.transition()
+                    conway.transition()
                     generation += 1
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
-        Grid.update(dead=white, live=blue, surface=screen)
+        conway.update(dead=white, live=blue, surface=screen)
+        pygame.display.update()
+
+
+def rps_game(random):
+    # Initialize field
+    screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
+    # screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+    screen.fill(black)
+    rps.update(rock=orange, paper=light_blue, scissors=red, surface=screen)
+    rps.transition()
+    if random == "y" or random == "Y":
+        rps.random_field()
+
+    generation = 0
+    while True:
+        pygame.display.set_caption("Rock, Paper, Scissors Cellular Automata - Generation " + str(generation))
+        clock.tick(speed)
+        keys = pygame.key.get_pressed()
+
+        # Continually iterates through generations while space is held
+        if keys[pygame.K_SPACE]:
+            rps.transition()
+            generation += 1
+
+        # Resets board and generation to 0 using command Ctrl + R
+        if (keys[pygame.K_LCTRL] or keys[pygame.K_RCTRL]) and keys[pygame.K_r]:
+            rps.reset()
+            generation = 0
+            pygame.display.set_caption("Rock, Paper, Scissors Cellular Automata - Generation " + str(generation))
+
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                button, choice = event.button, 0
+                if keys[pygame.K_1]:  # Hold 1 for orange
+                    choice = -1
+                if keys[pygame.K_2]:  # Hold 2 for blue
+                    choice = 1
+                if keys[pygame.K_3]:  # Hold 3 for red
+                    choice = 2
+
+                if button == 1:  # change cell state with left click
+                    pos = pygame.mouse.get_pos()
+                    rps.click(pos, choice)
+                elif button == 3:  # iterate through next generation once right click
+                    rps.transition()
+                    generation += 1
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        rps.update(rock=orange, paper=light_blue, scissors=red, surface=screen)
         pygame.display.update()
 
 
 if __name__ == '__main__':
-    game()
+    mode, random = "", ""
+    while mode != "1" and mode != "2":
+        mode = input("Select mode (1: Conway's Game of Life, 2: Rock Paper Scissors Cellular Automata):  ")
+    while random != "y" and random != "n" and random != "Y" and random != "N":
+        random = input("Would you like a randomly generated field? (y/n): ")
+
+    if mode == "1":
+        conway_game(random)
+    elif mode == "2":
+        rps_game(random)
